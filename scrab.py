6447af0bcab9e54c9684c40c5aa36ee7455d61d4
77wiz77 <47847427+77wiz77@users.noreply.github.com>
2021-09-19 19:47:24 +0300

    Update scrab.py

--- scrab.py
+++ scrab.py
@@ -17,9 +17,10 @@ def csv_writer(data, path):
 def trade_spiders (max_page):
   page = 1
   while page <= max_page:
-    url = 'https://habr.com/ru/search/page1/?q=react&target_type=posts&order=relevance'
+    url = 'https://habr.com/ru/search/page{}/?q=react&target_type=posts&order=relevance'.format(page)
     source_code = requests.get(url)
     soup = BeautifulSoup(source_code.text, 'html.parser')
+    # pages = int(soup.select("tm-pagination__page"))[-1] сделать вывод с нескольких страниц
     nameList = soup.findAll('a', {'class':'tm-user-info__username'})
     titleList = soup.findAll('h2', {'class':'tm-article-snippet__title'})
     tagList = soup.findAll('div', {'class':'tm-article-snippet__hubs'})
@@ -28,7 +29,7 @@ def trade_spiders (max_page):
       print('\n' + 'Автор: ' + nameList[i].text)
       print('Заголовок: ' + titleList[i].text)
       print('Тег: ' + tagList[i].text)
-      print('Текст' + textList[i].text)
+      print('Текст: ' + textList[i].text)
       print('---------------------------------')
       data.append(
           [str(nameList[i].text.split(",")).strip()] 
@@ -37,7 +38,7 @@ def trade_spiders (max_page):
                   + [str(textList[i].text.split(",")).strip()])
     page+=1
     
-trade_spiders(2)
+trade_spiders(40)
 path = "output.csv"
 print(data)
 csv_writer(data, path)
